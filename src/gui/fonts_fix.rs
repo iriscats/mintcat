366b61ff778c73fae141141bec5d69e4cc35da0f
iriscat <1129737123@qq.com>
2024-11-11 17:24:06 +0800

    [fix] fix chinese display

---
+++ src/gui/fonts_fix.rs
@@ -0,0 +1,13 @@
+
+
+
+pub(crate) fn load_fonts(ctx: &egui::Context) {
+    let mut fonts = egui::FontDefinitions::default();
+    fonts.font_data.insert("my_font".to_owned(),
+    egui::FontData::from_static(include_bytes!("../../assets/HarmonyOS Sans SC Thin.ttf")));
+    fonts.families.get_mut(&egui::FontFamily::Proportional).unwrap()
+        .insert(0, "my_font".to_owned());
+    fonts.families.get_mut(&egui::FontFamily::Monospace).unwrap()
+        .push("my_font".to_owned());
+    ctx.set_fonts(fonts);
+}
