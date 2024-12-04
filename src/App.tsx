8e3ccfb78b034b21ba6c51f8a45a957f93d41112
iriscat <1129737123@qq.com>
2024-12-04 11:53:13 +0800

    [feat] Adding multi-language support.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import {Layout} from 'antd';
+import {Layout, ConfigProvider} from 'antd';
+import {I18nextProvider} from "react-i18next"
+
 import TitleBar from "./components/TitleBar.tsx";
 import StatusBar from "./components/StatusBar.tsx";
 import MenuBar from "./components/MenuBar.tsx";
 import ModListPage from "./pages/ModListPage.tsx";
+import defaultTheme from './themes/default.ts';
+import i18n from "./locales/i18n"
 
 import './App.css';
 
@@ -11,22 +15,28 @@ const {Header, Footer, Sider, Content} = Layout;
 
 
 const App: React.FC = () => (
-    <Layout className={"app"} style={{backgroundColor: 'transparent'}}>
-        <Header className={"app-header"}>
-            <TitleBar/>
-        </Header>
-        <Layout style={{backgroundColor: 'transparent'}}>
-            <Sider width="50px" style={{backgroundColor: 'transparent'}}>
-                <MenuBar/>
-            </Sider>
-            <Content style={{backgroundColor: 'transparent', borderRadius: "8px"}}>
-                <ModListPage/>
-            </Content>
-        </Layout>
-        <Footer style={{backgroundColor: 'transparent', height: "40px"}}>
-            <StatusBar/>
-        </Footer>
-    </Layout>
+    <I18nextProvider i18n={i18n}>
+        <ConfigProvider
+            theme={defaultTheme}
+        >
+            <Layout className={"app"}>
+                <Header className={"app-header"}>
+                    <TitleBar/>
+                </Header>
+                <Layout>
+                    <Sider width="50px">
+                        <MenuBar/>
+                    </Sider>
+                    <Content style={{borderRadius: "8px"}}>
+                        <ModListPage/>
+                    </Content>
+                </Layout>
+                <Footer style={{height: "40px"}}>
+                    <StatusBar/>
+                </Footer>
+            </Layout>
+        </ConfigProvider>
+    </I18nextProvider>
 );
 
 export default App;
