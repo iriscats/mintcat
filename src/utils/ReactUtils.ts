5fdaa09700a8e422939d3503909c14eecfcef0c4
iris <1129737123@qq.com>
2025-04-22 11:55:22 +0800

    [refactor] use Decorator to refactor HomePage.tsx

---
+++ src/utils/ReactUtils.ts
@@ -0,0 +1,23 @@
+// 定义自动bind的装饰器
+export function autoBind(
+    _target: any,
+    _propertyKey: string,
+    descriptor: TypedPropertyDescriptor<any>
+) {
+    const originalMethod = descriptor.value;
+
+    return {
+        configurable: true,
+        get() {
+            // 将方法绑定到实例
+            const bound = originalMethod.bind(this);
+            // 定义到实例上避免重复绑定
+            Object.defineProperty(this, _propertyKey, {
+                value: bound,
+                configurable: true,
+                writable: true
+            });
+            return bound;
+        }
+    } as TypedPropertyDescriptor<any>;
+}
