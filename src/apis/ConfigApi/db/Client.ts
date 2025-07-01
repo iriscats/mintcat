544d052602316f0751aebff3d463863729e16866
iris <1129737123@qq.com>
2025-07-01 22:59:14 +0800

    [feat] add sqlite db.

--- src/apis/ConfigApi/db/Client.ts
+++ src/apis/ConfigApi/db/Client.ts
@@ -5,7 +5,7 @@ let dbInstance: ReturnType<typeof drizzle> | null = null;
 
 export async function getDb() {
     if (!dbInstance) {
-        const db = await Database.load('sqlite:test.db');
+        const db = await Database.load('sqlite:mintcat.db');
         dbInstance = drizzle(db);
     }
     return dbInstance;
--- src/apis/ConfigApi/db/Schema.ts
+++ src/apis/ConfigApi/db/Schema.ts
@@ -1,7 +1,25 @@
 import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 
-export const users = sqliteTable("users", {
+
+
+export const modInfos = sqliteTable("mod_info", {
     id: integer("id").primaryKey({ autoIncrement: true }),
-    name: text("name").notNull(),
-    email: text("email").notNull(),
+    source: text("source"),
+    modId: integer("mod_id"),
+    name: text("name"),
 });
+
+
+export const profiles = sqliteTable("profiles", {
+    id: integer("id").primaryKey({ autoIncrement: true }),
+
+});
+
+
+export const profileDetails = sqliteTable("profile_details", {
+    id: integer("id").primaryKey({ autoIncrement: true }),
+
+});
+
+
+
