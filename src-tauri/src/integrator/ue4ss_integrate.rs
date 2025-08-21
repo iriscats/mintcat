b91b4fbfcea0f968d017ddab2ac51660fd35d833
iris <1129737123@qq.com>
2025-08-21 19:15:52 +0800

    [feat] add CSharp mod support

--- src-tauri/src/integrator/ue4ss_integrate.rs
+++ src-tauri/src/integrator/ue4ss_integrate.rs
@@ -16,12 +16,31 @@ pub fn install_ue4ss(install_path: &PathBuf) {
         let ue4ss_dll = include_bytes!("../../assets/UE4SSL.dll");
         fs::write(dll_path, ue4ss_dll).unwrap();
 
+        let ue4ss_runtime_dll = ue4ss_path.join("UE4SSL.Runtime.dll");
+        let ue4ss_runtime_dll_buff = include_bytes!("../../assets/UE4SSL.Runtime.dll");
+        fs::write(ue4ss_runtime_dll, ue4ss_runtime_dll_buff).unwrap();
+
+        let ue4ss_csharp_dll = ue4ss_path.join("UE4SSL.CSharp.dll");
+        let ue4ss_csharp_dll_buff = include_bytes!("../../assets/UE4SSL.CSharp.dll");
+        fs::write(ue4ss_csharp_dll, ue4ss_csharp_dll_buff).unwrap();
+
+        let ue4ss_runtime_json = ue4ss_path.join("UE4SSL.Runtime.runtimeconfig.json");
+        let ue4ss_runtime_json_buff = include_bytes!("../../assets/UE4SSL.Runtime.runtimeconfig.json");
+        fs::write(ue4ss_runtime_json, ue4ss_runtime_json_buff).unwrap();
+
         let proxy_dll_path = install_path.join("dwmapi.dll");
         let proxy_dll = include_bytes!("../../assets/dwmapi.dll");
         fs::write(proxy_dll_path, proxy_dll).unwrap();
 
         let mods_path = ue4ss_path.join("mods");
         fs::create_dir(mods_path).unwrap();
+
+        let csmods_path = ue4ss_path.join("csmods");
+        let ue4ss_framework_dll = csmods_path.join("UE4SSL.Framework.dll");
+        fs::create_dir(csmods_path).unwrap();
+
+        let ue4ss_framework_dll_buff = include_bytes!("../../assets/UE4SSL.Framework.dll");
+        fs::write(ue4ss_framework_dll, ue4ss_framework_dll_buff).unwrap();
     }
 }
 
